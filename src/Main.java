f1997f62d3f17d52e744ce44396df88fb0049214
sirijarz <sirijarmale@gmail.com>
2022-10-27 11:35:20 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter the input string: ex: 101110");
+      Scanner sc= new Scanner(System.in);
+      String input= sc.nextLine();
+
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ src/urinals.java
@@ -0,0 +1,2 @@
+public class urinals {
+}
