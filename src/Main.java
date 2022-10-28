4318a2a482d50c5f3941354f4877f8ef9b45c4e6
sjarmale <sjarmale@asu.edu>
2022-10-27 17:16:25 -0700

    Implemented goodString function to check if a string is valid or not

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Please enter the input string: ex: 101110");
-      Scanner sc= new Scanner(System.in);
-      String input= sc.nextLine();
-
-        System.out.println("Hello world!");
+        Scanner sc= new Scanner(System.in);
+        String input= sc.nextLine();
+        urinals obj= new urinals();
+        Boolean isGoodString = obj.goodString(input);
+        if (isGoodString) {
+            System.out.println(input + " is a good string.");
+        } else {
+            System.out.println(input + " is not a good string");
+        }
     }
 
 }
--- src/urinals.java
+++ src/urinals.java
@@ -1,2 +1,12 @@
+import java.util.*;
 public class urinals {
+    Boolean goodString(String str) {
+        Boolean isGood = true;
+        //Check for only 0s and 1s in the string
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '1' && str.charAt(i) != '0')
+                isGood = false;
+        }
+        return isGood;
+    }
 }
